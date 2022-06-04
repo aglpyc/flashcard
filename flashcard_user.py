88e8d918c6dc316c3a9fc4387007b5f4668c8ed7
mhmtnzly <mhmtnzly@gmail.com>
2022-06-05 01:25:38 +0200

    first adding files

---
+++ flashcard_user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, name, level = 1) -> None:
+        self.name = name
+        self.level = level
+        self.totalTime = '00:00:00'
+    def login(self):
+        pass
+    def registerUserStat(self):
+        pass
